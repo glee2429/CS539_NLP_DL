9a163a3115be878bc9b93ed83261a6829fbb56a7
Ga Young Lee <leegay@oregonstate.edu>
2021-01-11 14:50:00 -0800

    HW 1 review + 10K filing analysis idea

--- HW/CS539_HW1_Code/Vocabulary.py
+++ HW/CS539_HW1_Code/Vocabulary.py
@@ -1,4 +1,4 @@
-from collections import Counter 
+from collections import Counter
 from re import sub, compile
 import matplotlib.pyplot as plt
 import numpy as np
@@ -31,19 +31,18 @@ class Vocabulary:
 	###########################
 	def tokenize(self, text):
 		"""
-	    
+
 	    tokenize takes in a string of text and returns an array of strings splitting the text into discrete tokens.
 
-	    :params: 
+	    :params:
 	    - text: a string to be tokenize, e.g. "The blue dog jumped, but not high."
 
 	    :returns:
 	    - tokens: a list of strings derived from the text, e.g. ["the", "blue", "dog", "jumped", "but", "not", "high"] for word-level tokenization
-	    
-	    """ 
 
-		# REMOVE THIS ONCE YOU IMPLEMENT THIS FUNCTION
-		raise UnimplementedFunctionError("You have not yet implemented tokenize.")
+	    """
+		pattern = re.compiler(r'[A-Za-z]+[\w^\']*|[\w^\']*[A-Za-z]+[\w^\']*')
+		return pattern.sub(text.lower())
 
 
 
@@ -52,18 +51,18 @@ class Vocabulary:
 	###########################
 	def build_vocab(self,corpus):
 		"""
-	    
+
 	    build_vocab takes in list of strings corresponding to a text corpus, tokenizes the strings, and builds a finite vocabulary
 
 	    :params:
 	    - corpus: a list string to build a vocabulary over
 
-	    :returns: 
+	    :returns:
 	    - word2idx: a dictionary mapping token strings to their numerical index in the dictionary e.g. { "dog": 0, "but":1, ..., "UNK":129}
 	    - idx2word: the inverse of word2idx mapping an index in the vocabulary to its word e.g. {0: "dog", 1:"but", ..., 129:"UNK"}
 	    - freq: a dictionary of words and frequency counts over the corpus (including words not in the dictionary), e.g. {"dog":102, "the": 18023, ...}
 
-	    """ 
+	    """
 
 		# REMOVE THIS ONCE YOU IMPLEMENT THIS FUNCTION
 		raise UnimplementedFunctionError("You have not yet implemented build_vocab.")
@@ -74,12 +73,11 @@ class Vocabulary:
 	###########################
 	def make_vocab_charts(self):
 		"""
-	    
+
 	    make_vocab_charts plots word frequency and cumulative coverage charts for this vocabulary. See handout for more details
 
-	    
-	    """ 
+
+	    """
 
 	    # REMOVE THIS ONCE YOU IMPLEMENT THIS FUNCTION
 		raise UnimplementedFunctionError("You have not yet implemented make_vocab_charts.")
-
